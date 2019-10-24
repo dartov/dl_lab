cae1d011c91fa5774ffbeebcc318e844c59a6b66
dartov <anton.zadorozhniy@gmail.com>
2019-10-24 12:22:55 +0200

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,21 @@
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install jupyter pyspark matplotlib pandas
+
+git clone git@github.com:dartov/tpch-dbgen.git
+
+cd tpch-dbgen
+
+make
+
+./dbgen -vf
+
+mkdir ../source/
+
+mv *.tbl ../source 
+
+cd ../
+
+mkdir -p /tmp/datalake/warehouse
